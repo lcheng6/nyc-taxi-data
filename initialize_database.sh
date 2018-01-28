a0f50b916e025b5b9a20ecc7c90fde7c83e17a55
Liang Cheng <liangcheng6@gmail.com>
2018-01-28 13:59:48 -0500

    update

--- initialize_database.sh
+++ initialize_database.sh
@@ -4,6 +4,7 @@ createdb nyc-taxi-data
 
 psql nyc-taxi-data -f create_nyc_taxi_schema.sql
 
+#this command load data from shape file (ESRI) into the postgres sql 
 shp2pgsql -s 2263:4326 taxi_zones/taxi_zones.shp | psql -d nyc-taxi-data
 psql nyc-taxi-data -c "CREATE INDEX index_taxi_zones_on_geom ON taxi_zones USING gist (geom);"
 psql nyc-taxi-data -c "CREATE INDEX index_taxi_zones_on_locationid ON taxi_zones (locationid);"
