810826c2d6741c754d1a167efcb75c8e447f75b7
Liang Cheng <liangcheng6@gmail.com>
2018-02-26 02:02:09 -0500

    add

---
+++ PG_Connection_Parameters.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PGHOST=mynyctaxidb.cxshmucrqlb5.us-east-1.rds.amazonaws.com
+export PGPORT=5432
+export PGDATABASE=nyc-taxi-data
+export PGUSER=mynyctaxidb
+export PGPASSWORD=mynyctaxidb
+
