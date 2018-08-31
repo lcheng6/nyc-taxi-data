89cf02391cdd3e6063684c4480b40dbfce2955c9
Liang Cheng <liangcheng6@gmail.com>
2018-08-31 14:47:10 -0400

    redo pg parameters.

--- PG_Connection_Parameters.sh
+++ PG_Connection_Parameters.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export PGHOST=nyctaxidata.cxshmucrqlb5.us-east-1.rds.amazonaws.com
+export PGHOST=nyctaxidatapostgres104.cxshmucrqlb5.us-east-1.rds.amazonaws.com
 export PGPORT=5432
 export PGDATABASE=nyctaxidata
 export PGUSER=nyctaxidata
