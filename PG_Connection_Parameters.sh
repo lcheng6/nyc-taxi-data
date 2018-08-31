9f5907bf298f0ba0ac7ccd5b55309ecb99efabc3
Liang Cheng <liangcheng6@gmail.com>
2018-08-31 12:48:18 -0400

    correct typo in password file

--- PG_Connection_Parameters.sh
+++ PG_Connection_Parameters.sh
@@ -2,6 +2,6 @@
 
 export PGHOST=nyctaxidata.cxshmucrqlb5.us-east-1.rds.amazonaws.com
 export PGPORT=5432
-export PGDATABASE=nytaxidata
-export PGUSER=nytaxidata
-export PGPASSWORD=nytaxidata
+export PGDATABASE=nyctaxidata
+export PGUSER=nyctaxidata
+export PGPASSWORD=nyctaxidata
