9b2311d45491135450e781bd3cf140ea6450b0d1
Liang Cheng <liangcheng6@gmail.com>
2018-08-31 01:27:51 -0400

    small update in database connection param

--- PG_Connection_Parameters.sh
+++ PG_Connection_Parameters.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-export PGHOST=nyc-taxi-data.cxshmucrqlb5.us-east-1.rds.amazonaws.com
+export PGHOST=nyctaxidata.cxshmucrqlb5.us-east-1.rds.amazonaws.com
 export PGPORT=5432
-export PGDATABASE=nyc-taxi-data
+export PGDATABASE=nytaxidata
 export PGUSER=nytaxidata
 export PGPASSWORD=nytaxidata
 
