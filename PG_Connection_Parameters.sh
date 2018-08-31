cf9df3c6f7bb3da069d1473b2ae259eaa5f66d6f
Liang Cheng <liangcheng6@gmail.com>
2018-08-31 15:19:02 -0400

    correct database parameter

--- PG_Connection_Parameters.sh
+++ PG_Connection_Parameters.sh
@@ -2,6 +2,6 @@
 
 export PGHOST=nyctaxidatapostgres104.cxshmucrqlb5.us-east-1.rds.amazonaws.com
 export PGPORT=5432
-export PGDATABASE=nyctaxidata
+export PGDATABASE=nyc-taxi-data
 export PGUSER=nyctaxidata
 export PGPASSWORD=nyctaxidata
